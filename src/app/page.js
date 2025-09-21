cd58d2733981de0cc4bf445b5175c34fd57715d6
gss-patricia <gss.patricia@gmail.com>
2025-09-21 17:26:57 +0100

    video 3.1

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,7 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { API_BASE_URL, API_ENDPOINTS } from "../../lib/config";
-
-//Função para buscar categorias da API interna(BFF) - SSR PURO
-const fetchCategories = async () => {
-  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.CATEGORIES}`, {
-    cache: "force-cache",
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Erro ao buscar categorias: ${response.status} ${response.statusText}`
-    );
-  }
-
-  return await response.json();
-};
-
-const fetchProducts = async () => {
-  const response = await fetch(`${API_BASE_URL}/${API_ENDPOINTS.PRODUCTS}`, {
-    cache: "no-store",
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Erro ao buscar produtos: ${response.status} ${response.statusText}`
-    );
-  }
-
-  return await response.json();
-};
+import { fetchCategories, fetchProducts } from "../../lib/data-layer";
 
 export default async function Home() {
   const [categorias, produtos] = await Promise.all([
